e9478f440760e8ab35e105bbb5b14797d2be528e
davidhardson <davidhardson@hotmail.com>
2017-04-20 13:27:44 +0800

    日常保洁回归2小时

--- baojie_richang_pay.js
+++ baojie_richang_pay.js
@@ -125,22 +125,22 @@ avalon.ready(function() {
         $id: "root",
         page:"main",
         item:"",
-    	hours:4,
+    	hours:2,
     	amount:0,
     	serviceItemId:6,
     	realMoney:0,
     	realAmount:0,
     	addhours:function(){
-    	    if(!o.hours||o.hours<4){
-    	        o.hours=4;
+    	    if(!o.hours||o.hours<2){
+    	        o.hours=2;
     	    } else {
     	        o.hours+=0.5;
     	    }
     	    computeAmount();
     	},
     	subhours:function(){
-    	    if(!o.hours||o.hours<=4){
-                o.hours=4;
+    	    if(!o.hours||o.hours<=2){
+                o.hours=2;
             } else {
                 o.hours-=0.5;
             }
